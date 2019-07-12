20e260a3450e05e7db5d42340a38ca57777a6e12
Ryan Hodder <i_ryan.h@hotmail.com>
2019-07-12 11:13:35 -0400

    Initial commit

---
+++ src/main/java/com/rev/BACalculator/BaCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.rev.BACalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rev/BACalculator/BaCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rev.BACalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaCalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
