821c7e83b48bd0a9cd69f7c0c64c6e5e2e59b9cb
Ryan Hodder <i_ryan.h@hotmail.com>
2019-07-16 15:35:43 -0400

    Changed a method to GetMapping

--- src/main/java/com/rev/BACalculator/controllers/DrinkController.java
+++ src/main/java/com/rev/BACalculator/controllers/DrinkController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,7 +23,7 @@ public class DrinkController {
 	@Autowired
 	private DrinkService drinkService;
 	
-	@PostMapping("/getDrinks.do")
+	@GetMapping("/getDrinks.do")
 	public @ResponseBody String getDrinks()
 	{
 		System.out.println("DrinkController->calculate");
